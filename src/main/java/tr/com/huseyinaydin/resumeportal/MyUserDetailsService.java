f8ed8d3be89c0f737f577a5245f0f870091ad77d
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-12-16 19:45:14 +0300

    Uygulama güvenliği için Spring Security ile kullanıcı detayları.
    Spring Security için özelleştirilmiş bir kullanıcı doğrulama ve yetkilendirme sistemi geliştirdim. İlk olarak, MyUserDetailsService sınıfını yazarak kullanıcı adını veri tabanında arayan ve kullanıcı bilgilerini yükleyen bir servis oluşturdum. Daha sonra, MyUserDetails sınıfını implemente ederek Spring Security'nin ihtiyaç duyduğu kullanıcı bilgilerini ve rollerini sağlayacak bir yapı kurdum. Bu sayede, kullanıcıların oturum açma ve yetkilendirme işlemlerini dinamik ve güvenilir bir şekilde gerçekleştirdim.

---
+++ src/main/java/tr/com/huseyinaydin/resumeportal/MyUserDetailsService.java
@@ -0,0 +1,36 @@
+package tr.com.huseyinaydin.resumeportal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import tr.com.huseyinaydin.resumeportal.models.MyUserDetails;
+import tr.com.huseyinaydin.resumeportal.models.User;
+
+import java.util.Optional;
+
+//بسم الله الرحمن الرحيم
+
+/**
+ *
+ * @author Huseyin_Aydin
+ * @since 1994
+ * @category Java, Spring Boot
+ *
+ */
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Kullanıcı bulunamadı kardeşim hadi git: " + userName));
+
+        return user.map(MyUserDetails::new).get();
+    }
+}
