4cb7b88d426deda450a32c1e36070ccab2ff9e0c
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-12-16 19:24:23 +0300

    Özgeçmiş portalı projesi oluşturuldu. User Entity sınıfı veritabanında tabloya karşılık gelecek şekilde dipnotlar ile eşlendi. User sınıfı kullanıcıların register ve login olabilmesi için.

---
+++ src/main/java/tr/com/huseyinaydin/resumeportal/HomeController.java
@@ -0,0 +1,18 @@
+package tr.com.huseyinaydin.resumeportal;
+
+import org.springframework.stereotype.Controller;
+
+//بسم الله الرحمن الرحيم
+
+/**
+ *
+ * @author Huseyin_Aydin
+ * @since 1994
+ * @category Java, Spring Boot
+ *
+ */
+
+@Controller
+public class HomeController {
+
+}
