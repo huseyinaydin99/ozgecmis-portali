dae5689a72ab9fe65cad3362797158e6b38f6872
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-12-16 20:00:47 +0300

    Uygulama güvenliği için Spring Security ayarlamaları.
    Spring Security için bir güvenlik yapılandırması oluşturdum. SecurityConfiguration sınıfında, kullanıcı doğrulama işlemini UserDetailsService ile entegre ettim ve kullanıcı şifrelerini geçici olarak düz metin halinde işlemek için NoOpPasswordEncoder kullandım. Ayrıca, /edit ve /user endpointlerine sadece oturum açmış kullanıcıların erişmesine izin verirken, diğer tüm endpointlere herkesin erişebilmesini sağladım.
    db.sql dosyasında ise insert into sql komutları ile kullanıcıları database'e girdim.

---
+++ src/main/resources/db.sql
@@ -0,0 +1,8 @@
+insert into users (id, user_name, password, active, roles) values
+(1, 'huseyinaydin99', 'toor', true, 'USER'),
+(2, 'hitler', 'Adolf', true, 'USER'),
+(3, 'stalin', 'Stalin', true, 'USER');
+
+insert into user_profiles (id, user_name, theme, summary, first_name, last_name, email, phone, designation) values
+(1, 'huseyinaydin99', 1, '11 Eylül 94 Niğde doğumlu. Bilgisayar programcısı Java Developer. 30 yaşında bekar. Sigara kullanmaz.', 'Hüseyin', 'Aydın', 'huseyinaydin99@gmail.com', '0555 555 55 51', 'Java Developer'),
+(2, 'stalin', 2, 'Josef Stalin, 1924-1953 yılları arasında Sovyetler Birliği\'nin liderliğini yapmış ve ülkeyi totaliter bir rejimle yönetmiştir. Endüstrileşme, kolektivizasyon politikaları ve İkinci Dünya Savaşı\'ndaki rolüyle tanınırken, aynı zamanda baskıcı yönetimi, sürgünler ve milyonlarca insanın ölümüne yol açan politikalarıyla da eleştirilmiştir.', 'Josef', 'Stalin', 'huseyinaydin99@gmail.com', '147-741-2123', 'Sovyetler Birliği Diktatörü');
