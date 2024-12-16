077084d9523a4ef658e6208bdd21ec95caea7a4a
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-12-16 20:17:07 +0300

    Thymeleaf şablon motoru ile profil HTML sayfası ve ilgili Controller.
    Kullanıcı profili görüntülemek için bir yapı geliştirdim. HomeController sınıfında, @PathVariable ile URL'den kullanıcı ID'sini alıp modeli kullanarak bu bilgiyi Thymeleaf şablonuna ilettim. Şablon dosyasında, kullanıcı ID'sini dinamik olarak göstermek için th:text özelliğini kullandım ve kullanıcıya özel bir profil başlığı oluşturdum.

--- src/main/java/tr/com/huseyinaydin/resumeportal/HomeController.java
+++ src/main/java/tr/com/huseyinaydin/resumeportal/HomeController.java
@@ -1,6 +1,11 @@
 package tr.com.huseyinaydin.resumeportal;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.security.Principal;
 
 //بسم الله الرحمن الرحيم
 
@@ -15,4 +20,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class HomeController {
 
+    @GetMapping("/view/{userId}")
+    public String view(@PathVariable String userId, Model model) {
+        model.addAttribute("userId", userId);
+        return "profile";
+    }
 }
