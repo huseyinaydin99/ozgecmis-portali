962081d29b2b7848e5289a66026f6ff1e728fbfb
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-12-17 13:57:28 +0300

    Üç adet profil template/tema eklendi.
    ResumPortal uygulamamda, kullanıcı deneyimini artırmak için Spring Boot ve Thymeleaf kullanarak üç farklı tema ekledim. Temalar arasında dinamik geçiş sağlamak adına tema seçimini kullanıcıya bırakan bir yapı geliştirdim. Ayrıca, her bir temayı modüler ve yeniden kullanılabilir şekilde tasarlayarak uygulamanın görselliğini ve esnekliğini artırdım.

--- src/main/java/tr/com/huseyinaydin/resumeportal/HomeController.java
+++ src/main/java/tr/com/huseyinaydin/resumeportal/HomeController.java
@@ -23,6 +23,6 @@ public class HomeController {
     @GetMapping("/view/{userId}")
     public String view(@PathVariable String userId, Model model) {
         model.addAttribute("userId", userId);
-        return "profile";
+        return "profile-templates/1/index";
     }
 }
